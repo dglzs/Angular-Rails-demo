69c92915e3db5ad8cd952dab08b5cf00bfa9e0a8
dglzs <dglzs@qq.com>
2016-03-23 19:17:17 +0800

    use ui-router and add comments

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-angular.module('flapperNews', [])
-.controller('MainCtrl', ['$scope', function($scope) {
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: '/home',
+    templateUrl: '/home.html',
+    controller: 'MainCtrl'
+  });
+  $stateProvider.state('posts', {
+    url: '/posts/{id}',
+    templateUrl: '/posts.html',
+    controller: 'PostCtrl'
+  });
+  $urlRouterProvider.otherwise('home');
+}]);
+
+app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
   $scope.test = 'Hello world!';
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 3},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4},
-  ];
+  $scope.posts = posts.posts;
   $scope.addPost = function() {
     if(!$scope.title || $scope.title === '') { return; }
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Leo', body: 'Nice, bro.', upvotes: 3}
+      ]
     });
     $scope.title = '';
     $scope.link = '';
@@ -23,3 +36,29 @@ angular.module('flapperNews', [])
   }
 }
 ]);
+
+app.controller('PostCtrl', ['$scope', '$stateParams', 'posts', function($scope, $stateParams, posts) {
+  $scope.post = posts.posts[$stateParams.id];
+  $scope.addComment = function() {
+    if($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
+}]);
+
+app.factory('posts', [function() {
+  var o = {
+    posts: [
+      {title: 'post 1', upvotes: 5, link:'', comments: []},
+      {title: 'post 2', upvotes: 3, link:'', comments: []},
+      {title: 'post 3', upvotes: 15, link:'', comments: []},
+      {title: 'post 4', upvotes: 9, link:'', comments: []},
+      {title: 'post 5', upvotes: 4, link:'', comments: []}
+    ]
+  };
+  return o;
+}]);
